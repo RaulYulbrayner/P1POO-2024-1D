f14183a50f069e7abcc3d478694042c0aaa395b7
RaulYulbrayner <153127665+RaulYulbrayner@users.noreply.github.com>
2024-02-15 15:53:18 -0500

    Rq01

--- colegio/src/main/java/co/edu/uniquindio/poo/Estudiante.java
+++ colegio/src/main/java/co/edu/uniquindio/poo/Estudiante.java
@@ -1,5 +1,13 @@
 package co.edu.uniquindio.poo;
 
-public record Estudiante(String nombre, String apellido, String identificacion, String correo, String telefono, byte edad) {
+public record Estudiante(String nombre, String apellido, String identificacion, String correo, String telefono, int edad) {
+
+    public Estudiante{
+        assert nombre != null;
+        assert apellido != null;
+        assert identificacion != null;
+        assert correo != null;
+        assert telefono != null;
+    }
     
 }
--- colegio/src/test/java/co/edu/uniquindio/poo/AppTest.java
+++ colegio/src/test/java/co/edu/uniquindio/poo/AppTest.java
@@ -8,7 +8,8 @@
 package co.edu.uniquindio.poo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import java.util.logging.Logger;
 import org.junit.jupiter.api.Test;
 
@@ -25,15 +26,38 @@ public class AppTest {
     public void datosCompletos() {
         LOG.info("Iniciado test datosCompletos");
         
-        Estudiante estudiante = new Estudiante("Camila", "Alzate Rios", "109453264", "camila@uniquindio.edu.co", "315635674", (byte) 18);
+        Estudiante estudiante = new Estudiante("Camila", "Alzate Rios", "109453264", "camila@uniquindio.edu.co", "315635674", 18);
 
         assertEquals("Camila", estudiante.nombre());
         assertEquals("Alzate Rios", estudiante.apellido());
         assertEquals("109453264", estudiante.identificacion());
         assertEquals("camila@uniquindio.edu.co", estudiante.correo());
         assertEquals("315635674", estudiante.telefono());
-        assertEquals((byte) 18, estudiante.edad());
+        assertEquals( 18, estudiante.edad());
 
         LOG.info("Finalizando test datosCompletos");
     }
+
+    /**
+     * Prueba para verificar que los datos de un estudiante no esten nulos
+     */
+    @Test
+    public void datosNulos(){
+        LOG.info("Inicio prueba datosNulos");
+        assertThrows(Throwable.class, ()-> new Estudiante(null, null, null, null, null, 0));
+        LOG.info("Finalizacion datosNulos");
+    }
+
+    /**
+     * Prueba para verificar que los datos de un estudiante no esten nulos
+     */
+    @Test
+    public void datosVacios(){
+        LOG.info("Inicio prueba datosVacios");
+        assertThrows(Throwable.class, ()-> new Estudiante("", "", "", "", "", 0));
+        LOG.info("Finalizacion datosVacios");
+    }
+
+
+
 }
